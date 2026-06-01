15d93bda972855ebfce06bc72f2376b8372dd4f5
andymoore01 <130947765+andymoore01@users.noreply.github.com>
2026-06-01 18:57:46 -0300

    correcion para render

--- controllers/alumno.controller.js
+++ controllers/alumno.controller.js
@@ -1,5 +1,5 @@
 const fs = require('fs').promises
-const { AlumnoModel } = require('../models/alumno.model')
+const { AlumnoModel } = require('../models/alumno.model.ts')
 
 const getAlumnoAll = async (req, res) => {
   try {
