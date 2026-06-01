3d3ff93f68299ca12c1675bea27ed808b9fdfd0b
JoaquinPelizza <joaquinpelizzayt@gmail.com>
2026-06-01 18:44:56 -0300

    Refactor comments for clarity in validar-campos.js

--- middlewares/validar-campos.js
+++ middlewares/validar-campos.js
@@ -1,19 +1,18 @@
 const { validationResult } = require('express-validator')
 
-// este middleware agarra la petición (req) y se fija si hubo algún quilombo 
+// este middleware agarra la petición (req) y se fija si hubo algún error 
 // con las validaciones (los 'check') que armamos en las rutas.
 const validarCampos = (req, res, next) => {
   // validationResult junta todos los errores que vinieron en la petición
   const errors = validationResult(req)
   
-  // Si la lista de errores NO está vacía, es porque mandaron cualquiera
+  // Si la lista de errores NO está vacía, es porque se rejistró algún error
   if (!errors.isEmpty()) {
-    // Le clavamos un estado 400 (Bad Request) y le pateamos los errores al front
+    // Le ponemos un estado 400 (Bad Request)
     return res.status(400).json(errors)
   }
   
-  // Si está todo joya, le damos mecha con next() para que siga viaje 
-  // hasta el controlador correspondiente (onda postAlumno).
+  // Si está todo bien, usamos el next() 
   next()
 }
 
